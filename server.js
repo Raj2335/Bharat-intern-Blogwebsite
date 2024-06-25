55567dafb52c1be5f0c13342edf613cd19ac506d
Raj2335 <156575875+Raj2335@users.noreply.github.com>
2024-06-25 10:29:14 +0530

    CHECK

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
-const express = require('express');
-const articleRouter = require('./routes/articles');
-const Article= require('./models/article')
-const mongoose = require('mongoose');
-const methodOverride=require('method-override');
-const app = express();
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
 
-mongoose.connect('mongodb:// 127.0.0.1:27017/bharatInternDatabase', {
-    connectTimeoutMS: 30000, // 30 seconds
-    socketTimeoutMS: 45000, // 45 seconds
-    serverSelectionTimeoutMS: 30000, // 30 seconds
-    family: 4
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-});
+mongoose.connect('mongodb://localhost/bharatInternDatabase')
+app.set("views", "./viewS")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
 
-app.set('view engine', 'ejs');
-app.set("views", "./views")
-app.use(express.urlencoded({ extended:false}));
-app.use(methodOverride('_method'));
-
-app.get('/',async(req, res) => {
-    const articles = await Article.find().sort({ createdAt:'desc'})
-    res.render('articles/index', { articles:articles }); // Pass the articles array to the template
-});
+app.use('/articles', articleRouter)
 
 app.use('/articles', articleRouter);
 app.listen(3001, () => {
